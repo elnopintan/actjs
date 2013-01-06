d45338f1accd263102d72020d205388335800271
Ignacio Blasco <elnopintan@gmail.com>
2013-01-06 20:03:26 +0100

    style in example

--- examples/dragdropexample/src/dragdropexample/cljs/dragdrop.cljs
+++ examples/dragdropexample/src/dragdropexample/cljs/dragdrop.cljs
@@ -26,15 +26,13 @@
   (set! (.-left (.-style node)) (str posX  "px"))
   (set! (.-top (.-style node)) (str posY "px")))
   
-
 (declare Dragged)
 
 (defrecord Empty []
   act/Response
   (listen [this [drag {:keys [classes node top left posX posY]}]]
    "This is the default state for the drag and drop surface when no node is dragged.
-   When received a :drag event drags an object if it's draggable."
-   
+   When received a :drag event drags an object if it's draggable."   
           (cond          
            (and (= :drag drag) (classes "draggable")) (Dragged. node (- posX left) (- posY top))
            :else this)))
@@ -57,20 +55,18 @@
 
 ; Javascript events transformed to actor events.
 (set! (.-onload js/window) 
-	(fn []
-		(set! (.-onmousedown (.getElementById js/document "rectangle"))
-      (fn [event]
-        (.log js/console "dragging...")
-        (act/send! [:drag event] surface) false))
-
-		(set! (.-onmousemove (.getElementById js/document "rectangle"))
-      (fn [event]
-        (act/send! [:move event] surface)))
-
-		(set! (.-onmouseup (.getElementById js/document "rectangle"))
-      (fn [event]
-        (.log js/console "dropping...")
-        (act/send! [:drop event] surface) true))))
+      (fn []
+				(set! (.-onmousedown (.getElementById js/document "rectangle"))
+     			(fn [event]
+      			(.log js/console "dragging...")
+        		(act/send! [:drag event] surface) false))
+				(set! (.-onmousemove (.getElementById js/document "rectangle"))
+      		(fn [event]
+        		(act/send! [:move event] surface)))
+				(set! (.-onmouseup (.getElementById js/document "rectangle"))
+      		(fn [event]
+        		(.log js/console "dropping...")
+        		S(act/send! [:drop event] surface) true))))
 
 
 
