8ae5816694c134f0d6828764a4e181687c58a6b1
Ignacio Blasco <elnopintan@gmail.com>
2013-01-06 20:09:40 +0100

    style in example

--- examples/dragdropexample/src/dragdropexample/cljs/dragdrop.cljs
+++ examples/dragdropexample/src/dragdropexample/cljs/dragdrop.cljs
@@ -7,7 +7,6 @@
                 (partition-by #(= % " ") text)))))
 
 ;Extension for MouseEvent to use it as a hash-map"
-
 (extend-type js/MouseEvent
   ILookup
   (-lookup [this k]
@@ -31,8 +30,8 @@
 (defrecord Empty []
   act/Response
   (listen [this [drag {:keys [classes node top left posX posY]}]]
-   "This is the default state for the drag and drop surface when no node is dragged.
-   When received a :drag event drags an object if it's draggable."   
+  "This is the default state for the drag and drop surface when no node is dragged.
+  When received a :drag event drags an object if it's draggable."   
           (cond          
            (and (= :drag drag) (classes "draggable")) (Dragged. node (- posX left) (- posY top))
            :else this)))
@@ -40,10 +39,10 @@
 (defrecord Dragged [dragged-node deltaX deltaY]
   act/Response
   (listen [this [command {:keys [posX posY]}]]
-          "When an object is dragged this state records which node is dragged and the difference between
-  				node and mouse positions.
-  				:move event updates positions
-  				:drop event returns the surface to Empty state"
+  "When an object is dragged this state records which node is dragged and the difference between
+  node and mouse positions.
+  :move event updates positions
+  :drop event returns the surface to Empty state"
            (do 
             (change-pos! dragged-node (- posX deltaX) (- posY deltaY))
           	(cond
@@ -56,17 +55,17 @@
 ; Javascript events transformed to actor events.
 (set! (.-onload js/window) 
       (fn []
-				(set! (.-onmousedown (.getElementById js/document "rectangle"))
-     			(fn [event]
-      			(.log js/console "dragging...")
-        		(act/send! [:drag event] surface) false))
-				(set! (.-onmousemove (.getElementById js/document "rectangle"))
-      		(fn [event]
-        		(act/send! [:move event] surface)))
-				(set! (.-onmouseup (.getElementById js/document "rectangle"))
-      		(fn [event]
-        		(.log js/console "dropping...")
-        		S(act/send! [:drop event] surface) true))))
+	(set! (.-onmousedown (.getElementById js/document "rectangle"))
+     	   (fn [event]
+      		(.log js/console "dragging...")
+        	(act/send! [:drag event] surface) false))
+        (set! (.-onmousemove (.getElementById js/document "rectangle"))
+      	   (fn [event]
+        	(act/send! [:move event] surface)))
+        (set! (.-onmouseup (.getElementById js/document "rectangle"))
+      	   (fn [event]
+        	(.log js/console "dropping...")
+        	(act/send! [:drop event] surface) true))))
 
 
 
